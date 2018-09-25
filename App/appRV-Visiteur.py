87c59a87d2e305fff5b0b32c22c253c64e306e83
developpeur <developpeur@slam.sio.lla.fr>
2018-09-25 16:28:50 +0200

    Correction appRv_Visiteur.py

--- App/appRV-Visiteur.py
+++ App/appRV-Visiteur.py
@@ -108,7 +108,7 @@ def addEchantillonsOfferts( matricule , numRapport ) :
 	
 	reponse = make_response( '' )												
 	if numRapport != None :
-		reponse.headers[ 'Location' ] = '/rapports/echantillons/%s/%d' % ( unRapport[ 'matricule' ] , unRapport[ 'numero' ] )
+		reponse.headers[ 'Location' ] = '/rapports/echantillons/%s/%s' % ( matricule ,  numRapport )
 		reponse.status_code = 201
 	else :
 		reponse.status_code = 409
@@ -120,6 +120,6 @@ def addEchantillonsOfferts( matricule , numRapport ) :
 
 
 if __name__ == '__main__' :
-	app.run( debug = True , host = '0.0.0.0' , port = 5000 )
+	app.run( debug = True , host = 'localhost' , port = 5000 )
 
 
